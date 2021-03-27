d372442080d0b4422ce9e7a74e5fc9181ccb9c4c
ysho <ysho2@illinois.edu>
2021-03-27 05:59:16 -0500

    Issues with Delete Function in Backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,123 @@
-
-
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-app.get('/', (require, response) => {
- response.send("Hello world");
-})
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "SpotifyDatabase",
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// READ
+app.get("/api/get", (require, response) => {
+  // const sqlSelect = "Select * FROM Song";
+  const sqlSelect = "SELECT DISTINCT s.name, a.artist_name, al.album_name, s.country_chart, s.chart_rank, s.song_id, al.album_id, a.artist_id FROM ((Song s JOIN Sings si ON s.song_id = si.song_id) JOIN Album al ON s.album_id = al.album_id) JOIN Artist a ON si.artist_id = a.artist_id WHERE s.country_chart LIKE ?";
+  db.query(sqlSelect, "test", (err, result) => {
+      console.log(result);
+      console.log(err);
+      response.send(result);
+  });
+});
+
+// Create
+app.post("/api/insert", (require, response) => {
+  const songId = require.body.songId;
+  const songName = require.body.songName;
+  const artistId = require.body.artistId;
+  const artistName = require.body.artistName;
+  const artistType = require.body.artistType;
+  const albumId = require.body.albumId;
+  const albumName = require.body.albumName;
+  const releaseDate = require.body.releaseDate;
+  const duration = require.body.duration;
+  const countryChart = require.body.countryChart;
+  const chartRank = require.body.chartRank;
+
+  const sqlInsert = "INSERT INTO Sings(song_id, artist_id) VALUES (?,?);";
+  db.query(sqlInsert, [songId, artistId], (err, result) => {
+      console.log(err);
+  })
+  const sqlInsert1 = "INSERT INTO Artist(artist_id,artist_name, artist_type) VALUES (?, ?, ?)";
+  db.query(sqlInsert1, [artistId, artistName, artistType], (err, result) => {
+      console.log(err);
+  })
+  const sqlInsert2 = "INSERT INTO Album(album_id, album_name, release_date) VALUES (?, ?, ?)";
+  db.query(sqlInsert2, [albumId, albumName, releaseDate], (err, result) => {
+      console.log(err);
+  })
+  const sqlInsert3 = "INSERT INTO Song(song_id, name, duration, album_id, country_chart, chart_rank) VALUES (?, ?, ?, ?, ?, ?)";
+  db.query(sqlInsert3, [songId, songName, duration, albumId, countryChart, chartRank], (err, result) => {
+      console.log(err);
+  })
+});
+
+
+// Delete
+app.delete("/api/delete/:albumId/:artistId/:songId", (require, response) => {
+  const songId = require.params.songId;
+  const artistId = require.params.artistId;
+  const albumId = require.params.albumId;
+  
+
+  const sqlDelete = "DELETE FROM Artist WHERE artist_id LIKE ?";
+  db.query(sqlDelete, artistId, (err, result) => {
+      if (err) 
+      console.log(err);
+  })
+  const sqlDelete1 = "DELETE FROM Sings WHERE song_id LIKE ? AND artist_id LIKE ?";
+  db.query(sqlDelete1, songId, artistId, (err, result) => {
+    console.log("2nd delete")
+      if (err) 
+      console.log(err);
+  })
+  const sqlDelete2 = "DELETE FROM Album WHERE album_id LIKE ?";
+  db.query(sqlDelete2, albumId, (err, result) => {
+    console.log("3rd delete")
+      if (err) 
+      console.log(err);
+  })
+  const sqlDelete3 = "DELETE FROM Song WHERE song_id LIKE ?";
+  db.query(sqlDelete3, songId, (err, result) => {
+    console.log("4th delete")
+      if (err) 
+      console.log(err);
+  })
+});
+
+// Update
+app.put("/api/update/", (require, response) => {
+  const albumId = require.body.albumId;
+  const artistId = require.body.artistId;
+  const songId = require.body.songId;
+  const songName = require.body.songName;
+  const artistName = require.body.artistName;
+  const albumName = require.body.albumName;
+  const countryChart = require.body.countryChart;
+  const countryRank = require.body.countryRank;
+
+  const sqlUpdate = "UPDATE Artist SET artist_name = ? WHERE artist_id= ?";
+  db.query(sqlUpdate, [artistName,artistId], (err, result) => {
+      if (err) 
+      console.log(err);
+  })
+  const sqlUpdate1 = "UPDATE Song SET name = ?, country_chart = ?, chart_rank = ? WHERE song_id= ?";
+  db.query(sqlUpdate1, [songName,countryChart, countryRank, songId], (err, result) => {
+      if (err) 
+      console.log(err);
+  })
+  const sqlUpdate2 = "UPDATE Album SET album_name = ? WHERE album_id= ?";
+  db.query(sqlUpdate2, [albumName,albumId], (err, result) => {
+      if (err) 
+      console.log(err);
+  })
+});
 
 app.listen(3002, () => {
- console.log("running on port 3002");
+  console.log("running on port 3002");
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,211 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
 import Axios from 'axios';
+
 function App() {
- return (
- <div className="App">
- <h1> CRUD APPLICATIONS</h1>
- <div className="form">
- <label> Movie Name:</label>
- <input type="text" name="movieName" />
- <label> Review:</label>
- <input type="text" name="Review" />
+  const [songId, setSongId] = useState(''); 
+  const [songName, setSongName] = useState('');
+  const [artistId, setArtistId] = useState('');
+  const [artistName, setArtistName] = useState('');
+  const [artistType, setArtistType] = useState('');
+  const [albumId, setAlbumId] = useState('');
+  const [albumName, setAlbumName] = useState('');
+  const [releaseDate, setReleaseDate] = useState('');
+  const [duration, setDuration] = useState('')
+  const [countryChart, setCountryChart] = useState('');
+  const [chartRank, setChartRank] = useState('');
+  const [songCharts, setSongCharts] = useState([]);
+  
+  const [newSongName, setNewSongName] = useState("");
+  const [newArtistName, setNewArtistName] = useState("");
+  const [newAlbumName, setNewAlbumName] = useState("");
+  const [newCountryChart, setNewCountryChart] = useState("");
+  const [newCountryRank, setNewChartRank] = useState("");
 
-  <button> Submit</button>
+  useEffect(() => {
+    Axios.get('http://localhost:3002/api/get').then((response) => {
+      console.log(response.data)
+      setSongCharts(response.data)
+    })
+  },[])
 
- </div>
+  const submitNewSong = () => { 
+    Axios.post('http://localhost:3002/api/insert', {
+      songId: songId,
+      songName: songName,
+      artistId: artistId,
+      artistName: artistName,
+      artistType: artistType,
+      albumId: albumId,
+      albumName: albumName,
+      releaseDate: releaseDate,
+      duration: duration,
+      countryChart: countryChart,
+      chartRank: chartRank,
+    });
+    
+    setSongCharts([
+      ...songCharts,
+      {
+        songId: songId,
+        songName: songName,
+        artistId: artistId,
+        artistName: artistName,
+        artistType: artistType,
+        albumId: albumId,
+        albumName: albumName,
+        releaseDate: releaseDate,
+        duration: duration,
+        countryChart: countryChart,
+        chartRank: chartRank,
+      },
+    ]);
+  };
 
- </div>
- );
+  const deleteSong = (songId, artistId, albumId) => {
+    Axios.delete(`http://localhost:3002/api/delete/${albumId}/${artistId}/${songId}`
+      // data: {
+      //   songId: "$songId",
+      //   artistId: "$artistId",
+      //   albumId: "$albumId"
+      // }
+    );
+  };
+
+  const updateSong = (curralbumId, currartistId, currsongId) => {
+    Axios.put(`http://localhost:3002/api/update`, {
+      albumId: curralbumId,
+      artistId: currartistId,
+      songId: currsongId,
+      songName: newSongName,
+      artistName: newArtistName,
+      albumName: newAlbumName,
+      countryChart: newCountryChart,
+      countryRank: newCountryRank
+
+    });
+    setNewSongName("")
+    setNewArtistName("")
+    setNewAlbumName("")
+    setNewCountryChart("")
+    setNewChartRank("")
+  };
+
+  return (
+    <div className="App">
+      <h1> CRUD APPLICATIONS</h1>
+
+      <div className="form">
+        <div>
+          <label> Song ID:</label>
+          <input type="text" name="songId" onChange={(e) => {
+            setSongId(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Song:</label>
+          <input type="text" name="songName" onChange={(e) => {
+            setSongName(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Artist ID:</label>
+          <input type="text" name="artistId" onChange={(e) => {
+            setArtistId(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Artist:</label>
+          <input type="text" name="artistName" onChange={(e) => {
+            setArtistName(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Artist Type:</label>
+          <input type="text" name="artistType" onChange={(e) => {
+            setArtistType(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Album ID:</label>
+          <input type="text" name="albumId" onChange={(e) => {
+            setAlbumId(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Album Name:</label>
+          <input type="text" name="albumName" onChange={(e) => {
+            setAlbumName(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Release Date:</label>
+          <input type="text" name="releaseDate" onChange={(e) => {
+            setReleaseDate(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Duration:</label>
+          <input type="text" name="duration" onChange={(e) => {
+            setDuration(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Country Chart:</label>
+          <input type="text" name="countryChart" onChange={(e) => {
+            setCountryChart(e.target.value)
+          } }/>
+        </div>
+        <div>
+          <label> Chart Rank:</label>
+          <input type="text" name="chartRank" onChange={(e) => {
+            setChartRank(e.target.value)
+          } }/>
+        </div>
+
+        
+        <button onClick={submitNewSong}> Submit</button>
+
+        {songCharts.map((val) => {
+          return (
+            <div className = "card" border="2px">
+              <div border="2px">
+                <div display = "flex" > Song: {val.name} </div>
+                <div display = "flex"> Artist: {val.artist_name}</div>
+                <div display = "flex"> Album: {val.album_name}</div>
+                <div display = "flex"> Country: {val.country_chart}</div>
+                <div display = "flex"> Chart: {val.chart_rank}</div>
+              </div>
+              <button onClick={() => { deleteSong(val.song_id, val.artist_id, val.album_id) }}> Delete</button>
+              <input type="text" id="updateSongName" defaultValue = {val.name} onChange={(e) => {
+                setNewSongName(e.target.value)
+              } }/>
+              <input type="text" id="updateArtistName" defaultValue = {val.artist_name} onChange={(e) => {
+                setNewArtistName(e.target.value)
+              } }/>
+              <input type="text" id="updateAlbumName" defaultValue = {val.album_name} onChange={(e) => {
+                setNewAlbumName(e.target.value)
+              } }/>
+              <input type="text" id="updateCountryChart" defaultValue = {val.country_chart} onChange={(e) => {
+                setNewCountryChart(e.target.value)
+              } }/>
+              <input type="text" id="updateChartRank" defaultValue = {val.chart_rank} onChange={(e) => {
+                setNewChartRank(e.target.value)
+              } }/>
+              <button onClick={() => {
+                updateSong(val.album_id, val.artist_id, val.song_id)
+              }}> Update</button>
+              </div>
+          );
+          
+          ;
+        })}
+        
+
+      </div>
+      
+    </div>
+  );
 }
+
 export default App;
